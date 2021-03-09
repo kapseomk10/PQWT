565750e3c921e0160af1265fad90c2d09608104f
kapseomk10 <71516710+kapseomk10@users.noreply.github.com>
2021-03-09 16:02:29 +0530

    Update mainPy.py

--- mainPy.py
+++ mainPy.py
@@ -3,8 +3,20 @@ from datetime import datetime
 from urllib.request import Request, urlopen
 import certifi
 import ssl
+from twilio.rest import Client 
+import paho.mqtt.client as mqtt
+from threading import *
+import RPi.GPIO as GPIO 
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+account_sid = 'AC1766a4c99191f01313a3c3e5cccd2bc9' 
+auth_token = 'e0d2ee5fc1faffd491cf320590e3001c' 
+client = Client(account_sid, auth_token) 
 
 mydb = conn.connect(host="192.168.1.108",user="hello1",passwd="hello1", database="hms", auth_plugin='mysql_native_password')
+mycursor = mydb.cursor()
 
 def liveTime():
     now = datetime.now()
@@ -19,6 +31,7 @@ def serviceTime(pid1,task1):
     mycursor.execute(sqlx)
     myresult = mycursor.fetchone()
     waitT[task1].append(int(myresult[0]))
+    print(waitT[task1])
     return int(myresult[0])
 
 def diffTime(current_time,arrivalTime):
@@ -47,65 +60,207 @@ def addTime(current,arrival):
         timehr = currenth + arrivalh
         return str(timehr)+"."+str(timemin) 
 
-pid,task=tuple(input().split(" "))
-
 waitT = { 'Registration':[0],'Check Up':[0],'XRay':[0],'Injection':[0],'CT Scan':[0],'MRI Scan':[0],'Medicine':[0],'Payment':[0] }
+
 taski = {0:'Registration',1:'Check Up',2:'XRay',3:'Injection',4:'CT Scan',5:'MRI Scan',6:'Medicine',7:'Payment'}
 
-task=taski[int(task)]
+def mqttt():
+	# The callback for when the client receives a CONNACK response from the server.
+	def on_connect(clienti, userdata, flags, rc):
+		print("Connected with result code "+str(rc))
+	 
+		# Subscribing in on_connect() - if we lose the connection and
+		# reconnect then subscriptions will be renewed.
+		clienti.subscribe("CoreElectronics")
 
-mycursor = mydb.cursor()
-sql = "select * from patient where patientID='"+pid+"'"
-mycursor.execute(sql)
-myres = mycursor.fetchone()
-print(myres)
-if myres!=None:
-    sql1 = "select * from livetasks where patientID='"+pid+"' and task='"+task+"'"
-    mycursor.execute(sql1)
-    myresult = mycursor.fetchone()
-    print(myresult)
-    if myresult!=None:
-        if(myresult[10]=='waiting'):
-            live = liveTime()
-            wai = diffTime(live,myresult[9])
-            sql3 = "update livetasks set status='inservice',wt='"+wai+"',sat='"+live+"' where patientID='"+pid+"'"
-            print(sql3)
-            mycursor.execute(sql3)
-            mydb.commit()
-            stat = 'inservice'
-            print("https://hmsprojectdatabase.000webhostapp.com/updateData.php?pid="+pid)
-            urlopen("https://hmsprojectdatabase.000webhostapp.com/updateData.php?pid="+pid, context=ssl.create_default_context(cafile=certifi.where()))
-        elif(myresult[10]=="inservice"):
-            sql4 = "Delete from livetasks where patientID='"+pid+"'"
-            print(sql4)
-            mycursor.execute(sql4)
-            mydb.commit()
-            print("https://hmsprojectdatabase.000webhostapp.com/removeData.php?pid="+pid)
-            urlopen("https://hmsprojectdatabase.000webhostapp.com/removeData.php?pid="+pid, context=ssl.create_default_context(cafile=certifi.where()))
-            #for completedtasks
-            arrivalTime = myresult[9]
-            endTime = liveTime()
-            wto1 = myresult[11]
-            sto1 = diffTime(endTime,myresult[12])
-            sqlform = "insert into completedtasks(patientID,name,age,gender,task,doctorID,wt,st,tts,at,et) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-            patientinfo = [(myres[0],myres[1],myres[2],myres[3],task,'1',wto1,sto1,addTime(wto1,sto1),arrivalTime,endTime)]
-            mycursor.executemany(sqlform,patientinfo)
-            mydb.commit()
-            sql5 = "Delete from patient where patientID='"+pid+"'"
-            print(sql5)
-            mycursor.execute(sql4)
-            mydb.commit()
-    else:
-        arrivalTime = liveTime()
-        wto = waitTime(task)
-        sto = serviceTime(pid,task)
-        sqlform = "insert into livetasks(patientID,name,age,gender,task,doctorID,pwt,pst,tts,at,status) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        patientinfo = [(myres[0],myres[1],myres[2],myres[3],task,'1',wto,sto,wto+sto,arrivalTime,'waiting')]
-        mycursor.executemany(sqlform,patientinfo)
-        mydb.commit()
-        stat = 'waiting'
-        print("https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid="+pid+"&task="+task.replace(' ','')+"&status="+stat+"&wt="+str(wto)+"&st="+str(sto))
-        urlopen("https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid="+pid+"&task="+task.replace(' ','')+"&status="+stat+"&wt="+str(wto)+"&st="+str(sto), context=ssl.create_default_context(cafile=certifi.where()))
-else: 
-    print('invalid input')
-    #https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid=1&task=Injection&status=waiting&wt=0&st=14
+	def on_message(clienti, userdata, msg):
+		an = str(msg.payload)[2:-3] 
+		print(msg.topic+" "+an)
+		onion(an)
+		
+	 
+	# The callback for when a PUBLISH message is received from the server.
+	def onion(a):
+			#a = str(msg.payload)[2:-1]
+			#print(a)
+			#pid,task=tuple(a.split(" "))
+			id, text = reader.read()
+			print('Id: ',id)
+			print('Text: ',text)
+			mycursor = mydb.cursor()
+			sql = "select num from rfid where patientID='"+text+"'"
+			mycursor.execute(sql)
+			myres = mycursor.fetchone()
+			print(myres)
+			task = 3
+			task=taski[int(task)]
+			sql = "select * from patient where patientID='"+pid+"'"
+			mycursor.execute(sql)
+			myres = mycursor.fetchone()
+			print(myres)
+			if myres!=None:
+				sql1 = "select * from livetasks where patientID='"+pid+"' and task='"+task+"'"
+				mycursor.execute(sql1)
+				myresult = mycursor.fetchone()
+				print(myresult)
+				if myresult!=None:
+					if(myresult[10]=='waiting'):
+						live = liveTime()
+						wai = diffTime(live,myresult[9])
+						sql3 = "update livetasks set status='inservice',wt='"+wai+"',sat='"+live+"' where patientID='"+pid+"'"
+						print(sql3)
+						mycursor.execute(sql3)
+						mydb.commit()
+						stat = 'inservice'
+						print("https://hmsprojectdatabase.000webhostapp.com/updateData.php?pid="+pid)
+						urlopen("https://hmsprojectdatabase.000webhostapp.com/updateData.php?pid="+pid, context=ssl.create_default_context(cafile=certifi.where()))
+						bodya = 'You are in service of '+task+'now, it will take '+str(serviceTime(pid,task))+'min to get serve'
+						messagei = client.messages.create( 
+									from_='whatsapp:+14155238886',  
+									body=bodya,      
+									to='whatsapp:+917507265603' 
+								) 
+						print(bodya)
+					elif(myresult[10]=="inservice"):
+						sql4 = "Delete from livetasks where patientID='"+pid+"'"
+						print(sql4)
+						mycursor.execute(sql4)
+						mydb.commit()
+						print("https://hmsprojectdatabase.000webhostapp.com/removeData.php?pid="+pid)
+						urlopen("https://hmsprojectdatabase.000webhostapp.com/removeData.php?pid="+pid, context=ssl.create_default_context(cafile=certifi.where()))
+						bodya = 'You have completed '+task
+						messagei = client.messages.create( 
+									from_='whatsapp:+14155238886',  
+									body=bodya,      
+									to='whatsapp:+917507265603' 
+								) 
+						print(bodya)
+						#for completedtasks
+						arrivalTime = myresult[9]
+						endTime = liveTime()
+						wto1 = myresult[11]
+						sto1 = diffTime(endTime,myresult[12])
+						sqlform = "insert into completedtasks(patientID,name,age,gender,task,doctorID,wt,st,tts,at,et) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+						patientinfo = [(myres[0],myres[1],myres[2],myres[3],task,'1',wto1,sto1,addTime(wto1,sto1),arrivalTime,endTime)]
+						mycursor.executemany(sqlform,patientinfo)
+						mydb.commit()
+						sql5 = "Delete from patient where patientID='"+pid+"'"
+						print(sql5)
+						mycursor.execute(sql4)
+						mydb.commit()
+				else:
+					arrivalTime = liveTime()
+					wto = waitTime(task)
+					sto = serviceTime(pid,task)
+					sqlform = "insert into livetasks(patientID,name,age,gender,task,doctorID,pwt,pst,tts,at,status) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+					patientinfo = [(myres[0],myres[1],myres[2],myres[3],task,'1',wto,sto,wto+sto,arrivalTime,'waiting')]
+					mycursor.executemany(sqlform,patientinfo)
+					mydb.commit()
+					stat = 'waiting'
+					print("https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid="+pid+"&task="+task.replace(' ','')+"&status="+stat+"&wt="+str(wto)+"&st="+str(sto))
+					urlopen("https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid="+pid+"&task="+task.replace(' ','')+"&status="+stat+"&wt="+str(wto)+"&st="+str(sto), context=ssl.create_default_context(cafile=certifi.where()))
+					bodya = 'You have entered into '+task+' now, it will take '+str(wto)+'min to get into service'
+					messagei = client.messages.create( 
+									from_='whatsapp:+14155238886',  
+									body=bodya,      
+									to='whatsapp:+917507265603' 
+								) 
+					print(bodya)
+			else: 
+				print('invalid input')
+				#https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid=1&task=Injection&status=waiting&wt=0&st=14
+	 
+	clienti = mqtt.Client()
+	clienti.on_connect = on_connect
+	clienti.on_message = on_message
+
+	clienti.connect("test.mosquitto.org", 1883, 60)
+
+	clienti.loop_forever()
+
+def rfid():
+	while True:
+		pid,task=tuple(input().split(" "))
+		task=taski[int(task)]
+		mycursor = mydb.cursor()
+		sql = "select * from patient where patientID='"+pid+"'"
+		mycursor.execute(sql)
+		myres = mycursor.fetchone()
+		print(myres)
+		if myres!=None:
+			sql1 = "select * from livetasks where patientID='"+pid+"' and task='"+task+"'"
+			mycursor.execute(sql1)
+			myresult = mycursor.fetchone()
+			print(myresult)
+			if myresult!=None:
+				if(myresult[10]=='waiting'):
+					live = liveTime()
+					wai = diffTime(live,myresult[9])
+					sql3 = "update livetasks set status='inservice',wt='"+wai+"',sat='"+live+"' where patientID='"+pid+"'"
+					print(sql3)
+					mycursor.execute(sql3)
+					mydb.commit()
+					stat = 'inservice'
+					print("https://hmsprojectdatabase.000webhostapp.com/updateData.php?pid="+pid)
+					urlopen("https://hmsprojectdatabase.000webhostapp.com/updateData.php?pid="+pid, context=ssl.create_default_context(cafile=certifi.where()))
+					bodya = 'You are in service of '+task+'now, it will take '+str(serviceTime(pid,task))+'min to get serve'
+					message = client.messages.create( 
+								  from_='whatsapp:+14155238886',  
+								  body=bodya,      
+								  to='whatsapp:+917507265603' 
+							  ) 
+					print(bodya)
+				elif(myresult[10]=="inservice"):
+					sql4 = "Delete from livetasks where patientID='"+pid+"'"
+					print(sql4)
+					mycursor.execute(sql4)
+					mydb.commit()
+					print("https://hmsprojectdatabase.000webhostapp.com/removeData.php?pid="+pid)
+					urlopen("https://hmsprojectdatabase.000webhostapp.com/removeData.php?pid="+pid, context=ssl.create_default_context(cafile=certifi.where()))
+					bodya = 'You have completed '+task
+					message = client.messages.create( 
+								  from_='whatsapp:+14155238886',  
+								  body=bodya,      
+								  to='whatsapp:+917507265603' 
+							  ) 
+					print(bodya)
+					#for completedtasks
+					arrivalTime = myresult[9]
+					endTime = liveTime()
+					wto1 = myresult[11]
+					sto1 = diffTime(endTime,myresult[12])
+					sqlform = "insert into completedtasks(patientID,name,age,gender,task,doctorID,wt,st,tts,at,et) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+					patientinfo = [(myres[0],myres[1],myres[2],myres[3],task,'1',wto1,sto1,addTime(wto1,sto1),arrivalTime,endTime)]
+					mycursor.executemany(sqlform,patientinfo)
+					mydb.commit()
+					sql5 = "Delete from patient where patientID='"+pid+"'"
+					print(sql5)
+					mycursor.execute(sql4)
+					mydb.commit()
+			else:
+				arrivalTime = liveTime()
+				wto = waitTime(task)
+				sto = serviceTime(pid,task)
+				sqlform = "insert into livetasks(patientID,name,age,gender,task,doctorID,pwt,pst,tts,at,status) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+				patientinfo = [(myres[0],myres[1],myres[2],myres[3],task,'1',wto,sto,wto+sto,arrivalTime,'waiting')]
+				mycursor.executemany(sqlform,patientinfo)
+				mydb.commit()
+				stat = 'waiting'
+				print("https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid="+pid+"&task="+task.replace(' ','')+"&status="+stat+"&wt="+str(wto)+"&st="+str(sto))
+				urlopen("https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid="+pid+"&task="+task.replace(' ','')+"&status="+stat+"&wt="+str(wto)+"&st="+str(sto), context=ssl.create_default_context(cafile=certifi.where()))
+				bodya = 'You have entered into '+task+' now, it will take '+str(wto)+'min to get into service'
+				message = client.messages.create( 
+								from_='whatsapp:+14155238886',  
+								body=bodya,      
+								to='whatsapp:+917507265603' 
+							) 
+				print(bodya)
+		else: 
+			print('invalid input')
+			#https://hmsprojectdatabase.000webhostapp.com/add_data.php?pid=1&task=Injection&status=waiting&wt=0&st=14
+
+t1 = Thread(target=mqttt)
+t1.start()
+
+t2 = Thread(target=rfid)
+t2.start()
